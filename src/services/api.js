afc950cb5b35d8207896ad58a22789a3efb22b7b
Akash Yadav <ay6422852@gmail.com>
2025-09-28 12:37:00 -0700

    Updated changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mahila-shakti-yojana.vercel.app//api",
+  baseURL: "https://mahila-shakti-yojana.vercel.app/api",
   timeout: 10000,
 });
 
